dddabbc2e54a6894baa4f2e11c71ecb4afdc184e
Ankit Pasayat <apasayat@cisco.com>
2024-02-14 00:58:55 +0530

    initial changes

---
+++ app/new-peel.tsx
@@ -0,0 +1,23 @@
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export default function NewPeel() {
+  const addPeel = async (formData: FormData) => {
+    "use server";
+    console.log("working on server");
+    const title = String(formData.get("title"));
+    const supabase = createServerActionClient<Database>({ cookies });
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (user) {
+      await supabase.from("peels").insert({ title: title, user_id: user.id });
+    }
+  };
+
+  return (
+    <form action={addPeel}>
+      <input name="title" className="bg-red-400" />
+    </form>
+  );
+}
